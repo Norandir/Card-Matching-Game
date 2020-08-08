4d0bb3daba16a9315e96a46c07a9c715c0e78c94
Zachary Johnson <pendragos@gmail.com>
2020-08-08 15:14:53 -0400

    Basic code for starting hint

--- app/src/main/java/com/example/cardmemorygame/MainActivity.java
+++ app/src/main/java/com/example/cardmemorygame/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cardmemorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.os.Bundle;
 
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    Timer timer;
+
+    public void hint (){
+
+        timer = new Timer();
+        timer.schedule(new game(), 5000); //after 5 seconds timer will call method to start game
+    }
+
+    class game extends TimerTask{
+        public void run() {
+            //Add code to start game
+            timer.cancel(); //Terminate the timer thread
+        }
+    }
+   
 }
